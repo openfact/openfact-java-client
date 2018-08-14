7e1c0d79ed62ff997bd4dfb3aac0f16f59087c10
Carlos Feria <carlosthe19916@gmail.com>
2018-08-14 13:01:17 -0500

    add document services

--- src/main/java/org/openfact/client/OpenfactClientFactory.java
+++ src/main/java/org/openfact/client/OpenfactClientFactory.java
@@ -4,6 +4,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.openfact.client.oauth.BearerAuthFilter;
+import org.openfact.client.oauth.TokenManager;
 import org.openfact.client.services.DocumentsService;
 
 public class OpenfactClientFactory {
@@ -12,11 +13,17 @@ public class OpenfactClientFactory {
         // Just static methods
     }
 
+    public static TokenManager getTokenManager(String clientId, String tokenServerUrl, String offlineToken) {
+        TokenManager tokenManager = new TokenManager(clientId, tokenServerUrl);
+        tokenManager.setRefreshToken(offlineToken);
+        return tokenManager;
+    }
 
-    public static DocumentsService getDocumentsService(String openfactServerUrl, String offlineToken) {
+
+    public static DocumentsService getDocumentsService(String openfactServerUrl, TokenManager tokenManager) {
         ResteasyClient client = new ResteasyClientBuilder().connectionPoolSize(10).build();
         ResteasyWebTarget target = client.target(openfactServerUrl);
-        target.register(new BearerAuthFilter(offlineToken));
+        target.register(new BearerAuthFilter(tokenManager));
         return target.proxy(DocumentsService.class);
     }
 
--- src/test/java/org/openfact/client/OpenfactClientFactoryTest.java
+++ src/test/java/org/openfact/client/OpenfactClientFactoryTest.java
@@ -2,15 +2,27 @@ package org.openfact.client;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openfact.client.oauth.TokenManager;
 import org.openfact.client.services.DocumentsService;
+import org.openfact.client.services.representations.idm.DocumentRepresentation;
+
+import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.*;
 
 public class OpenfactClientFactoryTest {
 
     @Test
-    public void getDocumentsService() {
-        DocumentsService service = OpenfactClientFactory.getDocumentsService("http://miserver", "miToken");
+    public void test_shouldCreateTokenManager() {
+        TokenManager tokenManager = OpenfactClientFactory.getTokenManager("myClientId", "myTokenUrl", "myOfflineToken");
+        Assert.assertNotNull(tokenManager);
+    }
+
+    @Test
+    public void test_shouldCreateOpenfactClient() {
+        TokenManager tokenManager = OpenfactClientFactory.getTokenManager("myClientId", "http://myServerTokenUrl", "myRefreshToken");
+        DocumentsService service = OpenfactClientFactory.getDocumentsService("http://myOpenfactServerUrl", tokenManager);
         Assert.assertNotNull(service);
     }
+
 }
