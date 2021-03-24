57ebd979ea506492e7002faf7961029ea78929f7
ChanoHL <shanohl.sist@gmail.com>
2021-03-24 12:17:06 -0500

    Prepare release 1.2.5

--- release.sh
+++ release.sh
@@ -3,25 +3,24 @@
 echo "===================================="
 read -p "Enter Release: " releaseVersion
 read -p "Enter Next Release (SNAPSHOT): " nextVersion
-read -p "Enter Github Username: " username
-read -s -p "Enter Github Password: " password
+read -s -p "Enter Github Token: " token
 
 # Update to release
 mvn versions:set -DnewVersion="$releaseVersion"
 
 git add .
 git commit -m "Prepare release $releaseVersion"
-git push "https://$username:$password@github.com/openfact/openfact-java-client.git"
+git push "https://$token@github.com/openfact/openfact-java-client.git"
 
 # Create tag and push
 git tag "$releaseVersion"
-git push "https://$username:$password@github.com/openfact/openfact-java-client.git" --tags
+git push "https://$token@github.com/openfact/openfact-java-client.git" --tags
 
 # Create next snapshot
 mvn versions:set -DnewVersion="$nextVersion-SNAPSHOT"
 
 git add .
 git commit -m "Prepare next release $nextVersion-SNAPSHOT"
-git push "https://$username:$password@github.com/openfact/openfact-java-client.git"
+git push "https://$token@github.com/openfact/openfact-java-client.git"
 
 pause
